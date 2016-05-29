0418146f3f2f1fc08b6a257a516a3dbb0f7bc356
ChelseyM <m.chelsey@gmail.com>
2016-05-29 13:44:45 +0200

    Initial work for including acceleration

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,4 +1,10 @@
 #include "ofApp.h"
+#include "MSAPhysics3D.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace msa::physics;
 
 bool learning = false;
 bool training = false;
@@ -10,6 +16,27 @@ int numOutputs = 12;
 float inputVals[6];
 float outputVals[12];
 
+bool				mouseAttract	= false;
+bool				doMouseXY		= false;		// pressing left mmouse button moves mouse in XY plane
+bool				doMouseYZ		= false;		// pressing right mouse button moves mouse in YZ plane
+bool				doRender		= true;
+int					forceTimer		= false;
+
+float				rotSpeed		= 0;
+float				mouseMass		= 1;
+
+int             	width;
+int         		height;
+
+//ofImage				ballImage;
+World3D_ptr         world;
+Particle3D_ptr      controllerNodeA;
+Particle3D_ptr      controllerNodeB;
+Particle3D_ptr      quarkA;
+Particle3D_ptr      quarkB;
+
+
+
 
 void ofApp::randomizeVals() {
     for(int i = 0; i < numOutputs; i++) {
@@ -18,10 +45,89 @@ void ofApp::randomizeVals() {
     }
 }
 
+// Physics Defs
+#define UNITS(s)                (s * ofGetWidth() / 1280.0) // scale to uniform units
+#define	SPRING_MIN_STRENGTH		0.005
+#define SPRING_MAX_STRENGTH		0.1
+
+#define	SPRING_MIN_WIDTH		UNITS(1)
+#define SPRING_MAX_WIDTH		UNITS(3)
+
+#define NODE_MIN_RADIUS			UNITS(5)
+#define NODE_MAX_RADIUS			UNITS(15)
+
+#define MIN_MASS				1
+#define MAX_MASS				3
+
+#define MIN_BOUNCE				0.2
+#define MAX_BOUNCE				0.9
+#define	FIX_PROBABILITY			10		// % probability of a particle being fixed on creation
+
+#define FORCE_AMOUNT			UNITS(10)
+
+#define EDGE_DRAG				0.98
+
+#define	GRAVITY					1
+
+#define MAX_ATTRACTION			10
+#define MIN_ATTRACTION			3
+
+#define SECTOR_COUNT			1		// currently there is a bug at sector borders, so setting this to 1
+
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     midiOut.listPorts();
     midiOut.openPort(0);
+    
+    
+    // PHYSICS -------------------------------------------------
+    width = ofGetWidth();
+    height = ofGetHeight();
+    
+    // initialize our physics world
+    world = World3D::create();
+    
+    world->setGravity(ofVec3f(0, GRAVITY, 0));
+    
+    // set world dimensions, not essential, but speeds up collision
+    world->setWorldSize(ofVec3f(-width/2, -height, -width/2), ofVec3f(width/2, height, width/2));
+    world->setSectorCount(SECTOR_COUNT);
+    world->setDrag(0.97f);
+    world->setDrag(1);		// FIXTHIS
+    world->enableCollision();
+    
+    controllerNodeA = Particle3D::create();
+    controllerNodeB = Particle3D::create();
+    quarkA = Particle3D::create();
+    quarkB = Particle3D::create();
+    world->addParticle(controllerNodeA);
+    world->addParticle(controllerNodeB);
+    world->addParticle(quarkA);
+    world->addParticle(quarkB);
+    
+    world->makeSpring(controllerNodeA, quarkA, ofRandom(SPRING_MIN_STRENGTH, SPRING_MAX_STRENGTH), ofRandom(10, width/2));
+    world->makeSpring(controllerNodeB, quarkB, ofRandom(SPRING_MIN_STRENGTH, SPRING_MAX_STRENGTH), ofRandom(10, width/2));
+    
+//    
+//    
+//    ofBackground(255, 255, 255);
+//    ofSetVerticalSync(true);
+//    ofSetFrameRate(60);
+//    ballImage.load("ball.png");
+//    
+//    //       initScene();
+//    windowResized(ofGetWidth(), ofGetHeight());
+//    
+//    setupLighting();
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     // setting random seed
@@ -64,20 +170,49 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+//    width = ofGetWidth();
+//    height = ofGetHeight();
+//    
+//    // update the world!
+//    world->update();
+    
     while(receiver.hasWaitingMessages())
     {
         VectorFloat inputVector(numInputs);
+        VectorFloat quarkPos(numInputs);
+        
+        VectorFloat inputVecA(3);
+        VectorFloat inputVecB(3);
+        
+        ofVec3f inputVecA3f(3);
+        ofVec3f inputVecB3f(3);
         
         ofxOscMessage m;
         receiver.getNextMessage(m);
         
         string vals = "inputVals:";
         
+        
         for(int i = 0; i < numInputs; i++) {
             inputVals[i] = m.getArgAsFloat(i);
             inputVector[i] = inputVals[i];
             vals += " " + std::to_string(inputVals[i]);
+            
+            if (i < 3) {
+                inputVecA.push_back(inputVals[i] + 10000);
+            } else {
+                inputVecB.push_back(inputVals[i] + 10000);
+            }
+            
+            string invalA;
+            for(int i = 0; i < numInputs; i++) {
+                invalA += "\n" + std::to_string(inputVecA[i]);
+            }
+            
+            cout << invalA;
+            
         }
+
         
         if (m.getArgAsInt(numInputs) == 1 || learning==true){
             trainingData.addSample(inputVector, targetVector );
@@ -97,7 +232,24 @@ void ofApp::update(){
         }
         
         if( pipeline.getTrained() && running ){
-            pipeline.predict(inputVector);
+            for(int i = 0; i < 3; i++) {
+                inputVecA3f[i] = inputVecA[i];
+                inputVecB3f[i] = inputVecB[i];
+            }
+            
+            controllerNodeA->moveBy(inputVecA3f);
+            controllerNodeB->moveBy(inputVecB3f);
+            
+            ofVec3f qA = quarkA->getPosition();
+            ofVec3f qB = quarkB->getPosition();
+
+            for(int i = 0; i < 3; i++) {
+                quarkPos[i] = qA[i];
+                quarkPos[3+i] = qB[i];
+
+            }
+            
+            pipeline.predict(quarkPos);
             
             VectorFloat regressionData;
             regressionData = pipeline.getRegressionData();
@@ -128,6 +280,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
     // HUD
     
     // INPUT
